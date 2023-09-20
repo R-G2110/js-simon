1f92d5776dcc3f42666fb6025af5a9a68bf6c3e5
R-G2110 <argeldelacruz86@gmail.com>
2023-09-21 01:18:02 +0200

    [Add]: setInterval on file script

--- js/script.js
+++ js/script.js
@@ -1,24 +1,55 @@
-const btnStart = document.querySelector('#start-btn');
-const timer = document.querySelector('#timer');
-const randomNumbers = document.querySelector('#numeri-da-indovindare');
-const cardNumeriDaIndovinare = document.querySelector('#card-numeri-da-indovinare');
+const timer = document.getElementById('timer');
+const btnStart = document.getElementById('start-btn');
+const randomNumbers = document.getElementById('numeri-da-indovindare');
+const cardNumeriDaIndovinare = document.getElementById('card-numeri-da-indovinare');
+const cardResult = document.getElementById('card-risultato');
 
-let numeriDaIndovindare =[];
+let insertedNumbers;
+let comparedNumbers;
+
+
 const limite = 5;
 let counter = limite;
+timer.innerHTML = 'Timer: ' +limite;
+const randomArray = createRandomArray(5);
+console.log (randomArray);
 
 
-timer.innerHTML = 'Timer: '+ limite;
 btnStart.addEventListener('click', start);
-numeriDaIndovindare = createRandomArray(5);
-randomNumbers.innerHTML = numeriDaIndovindare;
+randomNumbers.innerHTML = randomArray.join(' - ');
+
+
+
+
+
+  /*****************************************************/
+ /*****************************************************/
+/*****************************************************/
 
 function start() {
   this.removeEventListener('click', start);
 	cardNumeriDaIndovinare.classList.remove('d-none');
-  const clock = setInterval(function() {
+	
+	let counter2 = 6;
+
+	const clock2 = setInterval(function() {
+    counter2--;
+    
+    if (counter2 === 0) {
+      clearInterval(clock)
+			const insertedNumbers = insertNumArray();
+			console.log (insertedNumbers);
+			comparedNumbers = compareArrays(randomArray, insertedNumbers);
+			console.log ('comparedNumbers: '+comparedNumbers);
+			cardResult.classList.remove('d-none');
+			let result = document.getElementById('risultato');
+			result.innerHTML = 'Hai indovinato '+comparedNumbers.length + ' numero/i su 5:  '+comparedNumbers.join(' - ');
+
+    }
+  }, 1000)
+	const clock = setInterval(function() {
     counter--;
-    timer.innerHTML = 'Timer: '+ counter;
+    timer.innerHTML = 'Timer: ' + counter;
     
     if (counter === 0) {
       clearInterval(clock)
@@ -27,6 +58,26 @@ function start() {
   }, 1000)
 }
 
+
+//paragone tra 2 array
+function compareArrays(array1, array2){
+	const array =[];
+	for (let i=0; i<array1.length;i++){
+		if(array1.includes(array2[i])){
+			array.push(array2[i]);
+		}
+	}
+	return array;
+}
+
+//inserimento 5 numeri con prompt
+function insertNumArray(){
+	const array =[];
+	for (let i=0; i<5; i++){
+		array[i] = parseInt(prompt('Inserisci il '+(i+1)+'° numero'));
+	}
+	return array;
+}
 
 //genera 5 numeri random e li salva in un array
 function createRandomArray(limitNumber){
@@ -41,11 +92,9 @@ function createRandomArray(limitNumber){
 	}
 	return array;
 }
-console.log(numeriDaIndovindare);
-randomNumbers.innerHTML = numeriDaIndovindare;
 
 //genera un numero random
 function randomIndex(min, max){
-	result = Math.ceil(Math.random() * (max - min + 1) + min);
+	result = Math.ceil(Math.random() * (max - min + 1));
 	return result;
 };
